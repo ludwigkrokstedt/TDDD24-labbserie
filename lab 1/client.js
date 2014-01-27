e5f0716ba4bcafbdea05abaced7b0a21b89c667a
Ludde K <ludwig@krokstedt.se>
2014-01-27 15:45:30 +0100

    created some javascript for the forms. The fields do not validate yet though.

--- lab 1/client.js
+++ lab 1/client.js
@@ -1,6 +1,46 @@
-displayView = function(){
-   // the code required to display a view
-};
-window.onload = function(){
-    //code that is executed as the page is loaded.
-};
+function initiate() {
+	console.log("welcome");
+	//console.log(document.getElementById('welcomeview').innerHTML);
+	if (isLoggedIn()) {
+		console.log("you are logged in");
+		}
+	else {
+		console.log("you are not logged in");
+		showWelcomeView();
+	}
+	
+}
+
+//copies the content of welcome view and puts it in the content tag.
+function showWelcomeView() {
+	console.log("loading welcome view...");
+	document.getElementById('content').innerHTML=(document.getElementById('welcomeview').innerHTML);
+
+}
+
+function showProfileView() {
+	console.log("loading profile view...");
+	//do like showwelcomeview... no html written yet.
+}
+	
+function isLoggedIn() {
+	//unimplemented
+	return false;
+}
+
+//shoud loop through all the fields of the form and make sure that they
+// aren't empty. Doesn't work yet though.
+//
+// Unfinished.
+function validate(form) {
+	
+	console.log("test");
+	inputs = form.getElementsByTagName('input');
+	
+	for ( i=0; i<inputs.lenght; ++index) {
+		console.log(i.value);
+	}
+	
+	return false;
+	
+}
